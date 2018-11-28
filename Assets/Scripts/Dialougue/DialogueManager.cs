53f69bc0a746e61879554913275d7901adc31f0b
Byron <byronandrews-salvietto@live.co.uk>
2018-11-28 11:07:57 +0000

    New scene and Scripts
    > three dialogue scrpits made for room 23 mainly
    > and screne added for testing

---
+++ Assets/Scripts/Dialougue/DialogueManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    private Queue<string> senteces;
+
+	void Start ()
+    {
+        senteces = new Queue<string>();
+	}
+	
+	
+}
