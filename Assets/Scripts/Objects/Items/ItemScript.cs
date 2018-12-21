1e1cf10fdffc5372105384bd2486088562321051
ShadowSneaker <danielsfoyle@gmail.com>
2018-12-21 14:22:47 +0000

    Player Update
    -Removed the Point Light on the player as it was causing light glitches.
    
    Texture Improvements.

--- Assets/Scripts/Objects/Items/ItemScript.cs
+++ Assets/Scripts/Objects/Items/ItemScript.cs
@@ -6,10 +6,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class ItemScript : ScriptableObject
 {
+    // Should this item be removed when it is used.
     public bool ConsumeOnUse = true;
+
+    // The sprite this item uses.
     public Sprite Image;
 
+    // The display name of this item.
     public string ItemName;
+
+    // How muchthis item is worth when selling.
+    public int SellValue;
+
+
 
 	public virtual void Use(Entity User)
     {
