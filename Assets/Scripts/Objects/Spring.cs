ed14f10316c8dac7a9b5bc57f2e1b400ab33f231
ShadowSneaker <danielsfoyle@gmail.com>
2018-12-23 03:14:29 +0000

    Level 9
    Started Level 9
    +Added SM_Spring
    +Added Spring Animation
    +Added SpringScript
    +Added SM_Spikes
    Reshaped SM_Platform2
    
    Minor fixes to some levels
    
    *Found bug with movement. When applied horizontal force the character will reset it instantly due to how movement works.

---
+++ Assets/Scripts/Objects/Spring.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    public float Strength = 25.0f;
+
+    private Animator Anim;
+
+    private Entity Hit;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        Anim = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Hit = collision.gameObject.GetComponent<Entity>();
+
+        if (Hit)
+        {
+            Anim.SetBool("Play", true);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        Hit = null;
+    }
+
+    // Fix player movement so that the MoveSideways function isn't called every frame.
+    public void AddForce()
+    {
+        if (Hit)
+        {
+            Debug.Log(transform.up * Strength);
+            Hit.GetComponent<Rigidbody2D>().AddForce(transform.up * Strength);
+        }
+    }
+
+
+    public void ResetAnim()
+    {
+        Anim.SetBool("Play", false);
+    }
+}
