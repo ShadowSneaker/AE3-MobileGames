14251b3004bff697aaafc0b9ea1695057b011728
Herobyron <Byronandrews-salvietto@live.co.uk>
2019-01-10 23:07:45 +0000

    All Rooms Finished
    Finally

---
+++ Assets/Scenes/Room 35/Statues.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statues : MonoBehaviour {
+
+    
+    public GameObject Statue;
+    public GameObject StatueGreen;
+
+    public DoorScript Door;
+
+    void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Door.Activate();
+
+        Statue.SetActive(false);
+        StatueGreen.SetActive(true);
+
+        
+    }
+}
---
+++ Assets/Scenes/Room 38/LadderScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LadderScript : MonoBehaviour {
+
+    public GameObject Up;
+    public GameObject Down;
+
+    public GameObject Player;
+
+	void Start ()
+    {
+        Up.SetActive(false);
+        Down.SetActive(false);
+	}
+	
+	
+	void Update () {
+		
+	}
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+
+            Up.SetActive(true);
+            Down.SetActive(true);
+        }
+       
+        
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        Up.SetActive(false);
+        Down.SetActive(false);
+    }
+
+
+
+    public void OnClickUp()
+    {
+        
+        Player.transform.localPosition = new Vector3(Player.transform.localPosition.x, Player.transform.localPosition.y + 40, Player.transform.localPosition.z);
+    }
+
+    public void OnClickDown()
+    {
+        Player.transform.localPosition = new Vector3(Player.transform.localPosition.x, Player.transform.localPosition.y - 40, Player.transform.localPosition.z);
+    }
+
+}
+
